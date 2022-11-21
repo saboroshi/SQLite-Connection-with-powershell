3c6659283831c46366c40fab35ea29eb4ee15343
saboroshi <38815641+saboroshi@users.noreply.github.com>
2022-11-21 22:00:53 +0100

    Add files via upload

---
+++ SQLite-Connection.psm1
@@ -0,0 +1,39 @@
+﻿<#	
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2021 v5.8.196
+	 Created on:   	2022. 10. 03. 22:02
+	 Created by:   	Cservenyi Szabolcs
+	 Organization: 	
+	 Filename:     	SQLite-Connection.psm1
+	-------------------------------------------------------------------------
+	 Module Name: SQLite-Connection
+	===========================================================================
+#>
+
+function SQLite-Connection
+{
+	param (
+		$DLLPath = "",
+		$DBfile = "",
+		$Query = ""
+	)
+	Add-Type -Path "$DLLPath"
+	$Connection = New-Object System.Data.SQLite.SQLiteConnection
+	$Connection.ConnectionString = "Data Source=$DBfile"
+	$Cmd = $Connection.CreateCommand()
+	$Cmd.CommandText = $Query
+	$Cmd.Connection = $Connection
+	$Adapter = New-Object System.Data.SQLite.SQLiteDataAdapter
+	$Adapter.SelectCommand = $Cmd
+	$DataSet = New-Object System.Data.DataSet
+	$Adapter.Fill($DataSet)
+	$DataSet.Tables[0]
+	$Connection.Close()
+}
+
+Export-ModuleMember SQLite-Connection
+
+
+
+
+
---
+++ SQLite-TestConnection.psm1
@@ -0,0 +1,43 @@
+﻿<#	
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2021 v5.8.196
+	 Created on:   	2022. 10. 03. 22:10
+	 Created by:   	Cservenyi Szabolcs
+	 Organization: 	
+	 Filename:     	SQLite-TestConnection.psm1
+	-------------------------------------------------------------------------
+	 Module Name: 	SQLite-TestConnection
+	===========================================================================
+#>
+
+function SQLite-TestConnection
+{
+	param (
+		$DLLPath = "",
+		$DBfile = "",
+		$Query = ""
+	)
+	$ErrorActionPreference = 'Stop'
+	try
+	{
+		Add-Type -Path "$DLLPath"
+		$Connection = New-Object System.Data.SQLite.SQLiteConnection
+		$Connection.ConnectionString = "Data Source=$DBfile"
+		$true
+	}
+	catch
+	{
+		$false
+	}
+	finally
+	{
+		$Connection.Close()
+	}
+}
+
+Export-ModuleMember SQLite-TestConnection
+
+
+
+
+
